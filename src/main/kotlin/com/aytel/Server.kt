367ac994166dd4c096191982456d05dc9247fa7d
aytel <arseny.babushkin@gmail.com>
2019-05-28 13:20:52 +0500

    Deploy

---
+++ src/main/kotlin/com/aytel/Server.kt
@@ -0,0 +1,66 @@
+package com.aytel
+
+import java.io.BufferedReader
+import java.net.Socket
+import java.io.OutputStream
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.ServerSocket
+
+
+class HttpServer {
+    fun main() {
+        val ss = ServerSocket(8080)
+        while (true) {
+            val s = ss.accept()
+            System.err.println("Client accepted")
+            Thread(SocketProcessor(s)).start()
+        }
+    }
+
+    private class SocketProcessor @Throws(Throwable::class)
+    constructor(private val s: Socket) : Runnable {
+        private val `is`: InputStream = s.getInputStream()
+        private val os: OutputStream = s.getOutputStream()
+
+        override fun run() {
+            try {
+                readInputHeaders()
+                writeResponse("<html><body><h1>Hello from Habrahabr</h1></body></html>")
+            } catch (t: Throwable) {
+                /*do nothing*/
+            } finally {
+                try {
+                    s.close()
+                } catch (t: Throwable) {
+                    /*do nothing*/
+                }
+
+            }
+            System.err.println("Client processing finished")
+        }
+
+        @Throws(Throwable::class)
+        private fun writeResponse(s: String) {
+            val response = "HTTP/1.1 200 OK\r\n" +
+                    "Server: YarServer/2009-09-09\r\n" +
+                    "Content-Type: text/html\r\n" +
+                    "Content-Length: " + s.length + "\r\n" +
+                    "Connection: close\r\n\r\n"
+            val result = response + s
+            os.write(result.toByteArray())
+            os.flush()
+        }
+
+        @Throws(Throwable::class)
+        private fun readInputHeaders() {
+            val br = BufferedReader(InputStreamReader(`is`))
+            while (true) {
+                val s = br.readLine()
+                if (s == null || s.trim { it <= ' ' }.isEmpty()) {
+                    break
+                }
+            }
+        }
+    }
+}
